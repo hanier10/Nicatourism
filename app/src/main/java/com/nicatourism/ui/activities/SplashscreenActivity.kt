b5e8df790c258ebbf07d7452c7f3975d93a9167c
Hanier Morales <hsmp16@gmail.com>
2020-11-27 09:40:41 -0600

    Creación de Fragments

---
+++ app/src/main/java/com/nicatourism/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.nicatourism.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nicatourism.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
